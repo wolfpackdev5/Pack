7966a0047e3c3e3e3691a48a33c3188c7405af96
Devin Lewis <dglewis@umich.edu>
2022-11-08 17:09:38 -0500

    pack spring boot version

---
+++ pack/src/main/java/wolfpack/pack/PackApplication.java
@@ -0,0 +1,13 @@
+package wolfpack.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackApplication.class, args);
+	}
+
+}
---
+++ pack/src/test/java/wolfpack/pack/PackApplicationTests.java
@@ -0,0 +1,13 @@
+package wolfpack.pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
